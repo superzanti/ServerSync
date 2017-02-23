b841651c7362b43f95ebc55d36db3ef3dce39fcf
Iessa Pragg <iessa.pragg@gmail.com>
2017-02-23 19:23:29 +1300

    #48 Add forge loader classes

---
+++ src/main/java/com/superzanti/serversync/forgeloader/ForgeLoaderNET.java
@@ -0,0 +1,23 @@
+package com.superzanti.serversync.forgeloader;
+
+import java.io.IOException;
+
+import com.superzanti.lib.RefStrings;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import runme.Main;
+
+@Mod(modid="com.superzanti.serversync", name="ServerSync", version=RefStrings.VERSION)
+public class ForgeLoaderNET {
+	@EventHandler
+	public void startServersync(FMLPreInitializationEvent _e) {
+			try {
+				Main.main(new String[]{"server"});
+			} catch (InterruptedException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+}
