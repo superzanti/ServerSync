565daca5566a7a6dd4f580bbf40dbe3a86659b3d
R <iessa.pragg@gmail.com>
2018-04-26 19:00:43 +1200

    #86 Clean formatting

--- src/main/java/com/superzanti/serversync/util/SyncFile.java
+++ src/main/java/com/superzanti/serversync/util/SyncFile.java
@@ -21,74 +21,85 @@ import com.google.gson.JsonStreamParser;
 import com.superzanti.serversync.util.errors.InvalidSyncFileException;
 
 /**
- * Holds all relevant information about a synchronizable file, also handles client only files
+ * Holds all relevant information about a synchronizable file, also handles
+ * client only files
  * 
  * @author Rheimus
  *
  */
 public class SyncFile implements Serializable {
 	private static final long serialVersionUID = -3869215783959173682L;
-	
+
 	public final boolean isConfigurationFile;
 	public final boolean isClientSideOnlyFile;
 	private MinecraftModInformation minecraftInformation;
+
 	public MinecraftModInformation getModInformation() {
 		return this.minecraftInformation;
 	}
-	
+
 	private final String fileHash;
+
 	public String getFileHash() {
 		return this.fileHash;
 	}
+
 	private final File synchronizableFile;
+
 	public String getFileName() {
 		return this.synchronizableFile.getName();
 	}
+
 	public File getFile() {
 		return this.synchronizableFile;
 	}
+
 	public Path getFileAsPath() {
 		return this.synchronizableFile.toPath();
 	}
+
 	public Path getClientSidePath() {
-		//TODO link this to a config value
+		// TODO link this to a config value
 		return Paths.get(this.synchronizableFile.getPath().replaceFirst("clientmods", "mods"));
 	}
-	
-	
+
 	/**
 	 * Factory for creating a config sync file
+	 * 
 	 * @param fileToSync
 	 * @return SyncFile file set up as a config
 	 */
 	public static SyncFile ConfigSyncFile(Path fileToSync) {
 		return new SyncFile(fileToSync, true, false);
 	}
-	
+
 	/**
 	 * Factory for creating a standard sync file
+	 * 
 	 * @param fileToSync
 	 * @return SyncFile set up as a standard file (normal mods)
 	 */
 	public static SyncFile StandardSyncFile(Path fileToSync) {
 		return new SyncFile(fileToSync, false, false);
 	}
-	
+
 	/**
 	 * Factory for creating a client side only sync file
+	 * 
 	 * @param fileToSync
-	 * @return SyncFile set up to be a client only file <br>(stuff the client might need but the server doesn't)
+	 * @return SyncFile set up to be a client only file <br>
+	 *         (stuff the client might need but the server doesn't)
 	 */
 	public static SyncFile ClientOnlySyncFile(Path fileToSync) {
 		return new SyncFile(fileToSync, false, true);
 	}
-	
+
 	private SyncFile(Path fileToSync, boolean isConfig, boolean isClientSideOnly) {
 		this.synchronizableFile = fileToSync.toFile();
 		this.isConfigurationFile = isConfig;
 		this.isClientSideOnlyFile = isClientSideOnly;
 		this.fileHash = Md5.md5String(this.synchronizableFile);
-		
+
 		if (!isConfig) {
 			this.populateModInformation();
 		}
@@ -107,7 +118,7 @@ public class SyncFile implements Serializable {
 	 */
 	public boolean matchesIncludeListPattern() {
 		FileIncludeMatcher includedFiles = new FileIncludeMatcher();
-		if (this.isConfigurationFile) {			
+		if (this.isConfigurationFile) {
 			return includedFiles.matches(this.getFileAsPath());
 		} else {
 			return true;
@@ -137,26 +148,27 @@ public class SyncFile implements Serializable {
 			InputStreamReader read = null;
 			JsonStreamParser parser = null;
 			JarFile packagedMod = null;
+
 			try {
 				ArrayList<String> infoTests = new ArrayList<String>(2);
 				infoTests.add("mcmod.info");
 				infoTests.add("neimod.info");
 				packagedMod = new JarFile(this.synchronizableFile);
 				JarEntry modInfo = null;
-				
+
 				for (String test : infoTests) {
 					modInfo = packagedMod.getJarEntry(test);
-					
+
 					if (modInfo != null) {
 						break;
 					}
 				}
-					
+
 				if (modInfo != null) {
 					is = packagedMod.getInputStream(modInfo);
 					read = new InputStreamReader(is);
 					parser = new JsonStreamParser(read);
-					
+
 					while (parser.hasNext()) {
 						JsonElement element = parser.next();
 						if (element.isJsonArray()) {
@@ -180,9 +192,7 @@ public class SyncFile implements Serializable {
 								////////////////////////////////////////////////
 
 								this.minecraftInformation = new MinecraftModInformation(
-																	info.get("version").getAsString(), 
-																	info.get("name").getAsString()
-																);
+										info.get("version").getAsString(), info.get("name").getAsString());
 							}
 						}
 					}
@@ -193,68 +203,73 @@ public class SyncFile implements Serializable {
 				}
 
 			} catch (JsonParseException e) {
-				System.out.println("File: " + this.synchronizableFile.getName() + " failed to parse mcmod.info as JSON");
+				System.out
+						.println("File: " + this.synchronizableFile.getName() + " failed to parse mcmod.info as JSON");
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
-					try {
-						if (read != null) read.close();
-						if (is != null) is.close();
-						if (packagedMod != null) packagedMod.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+				try {
+					if (read != null)
+						read.close();
+					if (is != null)
+						is.close();
+					if (packagedMod != null)
+						packagedMod.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		} else {
-			System.out.println("File: " + this.synchronizableFile.getName() + " not recognized as a minecraft mod (not a problem)");
+			System.out.println("File: " + this.synchronizableFile.getName()
+					+ " not recognized as a minecraft mod (not a problem)");
 		}
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
 		// Patch for using compare on lists with sync files
 		if (o instanceof SyncFile) {
 			try {
-				return this.equals((SyncFile)o);
+				return this.equals((SyncFile) o);
 			} catch (InvalidSyncFileException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return super.equals(o);
 	}
 
 	/**
-	 * Compares mod versions from mcmod.info or compares file contents if
-	 * version could not be found
+	 * Compares mod versions from mcmod.info or compares file contents if version
+	 * could not be found
 	 * 
 	 * @param serversMod
 	 *            - servers version of the mod
 	 * @return True if versions or content are the same<br>
-	 *         False if versions are different or if version is unknown and
-	 *         contents could not be read
+	 *         False if versions are different or if version is unknown and contents
+	 *         could not be read
 	 */
 	public boolean equals(SyncFile otherSyncFile) throws InvalidSyncFileException {
 		if (otherSyncFile == null) {
 			System.out.println("Attempted to compare a null SyncFile");
 			throw new InvalidSyncFileException();
 		}
-		
+
 		if (this.getFileName() == null || otherSyncFile.getFileName() == null) {
 			System.out.println("Could not get file names");
 			throw new InvalidSyncFileException();
 		}
-		
+
 		if (this.fileHash == null || otherSyncFile.fileHash == null) {
 			System.out.println("File hash comparison impossible");
 			System.out.println(this.getFileName() + " : " + otherSyncFile.getFileName());
 			throw new InvalidSyncFileException();
 		}
-		
+
 		if (!this.getFileName().equals(otherSyncFile.getFileName())) {
 			return false;
 		}
-		
+
 		if (otherSyncFile.minecraftInformation != null && this.minecraftInformation != null) {
 			return this.minecraftInformation.version.equals(otherSyncFile.minecraftInformation.version)
 					&& this.minecraftInformation.name.equals(otherSyncFile.minecraftInformation.name);
@@ -275,7 +290,8 @@ public class SyncFile implements Serializable {
 			// File holds resources, can't delete while it exists without using File methods
 			success = this.synchronizableFile.delete();
 		} catch (SecurityException e) {
-			System.out.println("Could not access file: " + this.synchronizableFile.getName() + " security violation check permissions");
+			System.out.println("Could not access file: " + this.synchronizableFile.getName()
+					+ " security violation check permissions");
 			e.printStackTrace();
 		}
 		if (!success) {
@@ -283,7 +299,7 @@ public class SyncFile implements Serializable {
 		}
 		return success;
 	}
-	
+
 	@SafeVarargs
 	public static ArrayList<String> listModNames(List<SyncFile>... modLists) {
 		if (modLists != null && modLists.length > 0) {
