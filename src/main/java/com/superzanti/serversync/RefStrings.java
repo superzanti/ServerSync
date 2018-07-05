1ec44e4e51d477eb60078525182465b00211da3b
Rheimus <iessa.pragg@gmail.com>
2018-07-05 19:25:11 +1200

    NoIssue: Bump version

--- src/main/java/com/superzanti/serversync/RefStrings.java
+++ src/main/java/com/superzanti/serversync/RefStrings.java
@@ -3,5 +3,5 @@ package com.superzanti.serversync;
 public class RefStrings {
 	public static final String MODID = "com.superzanti.serversync";
 	public static final String NAME = "ServerSync";
-	public static final String VERSION = "2.6.19";
+	public static final String VERSION = "2.6.20";
 }
