ad0d04e655c1a56a1c283f755c2efd51b13cb37d
Rheimus <iessa.pragg@gmail.com>
2020-03-31 20:43:59 +1300

    Bump minor version

--- src/main/java/com/superzanti/serversync/RefStrings.java
+++ src/main/java/com/superzanti/serversync/RefStrings.java
@@ -3,7 +3,7 @@ package com.superzanti.serversync;
 public class RefStrings {
     public static final String MODID = "com.superzanti.serversync";
     public static final String NAME = "ServerSync";
-    public static final String VERSION = "3.2.1";
+    public static final String VERSION = "3.3.0";
     public static final String ERROR_TOKEN = "<E>";
     public static final String DELETE_TOKEN = "<D>";
     public static final String UPDATE_TOKEN = "<U>";
