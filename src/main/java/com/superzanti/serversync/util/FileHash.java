530c349a8206d76fa7ba1a68362bd7eda625f4af
Rheimus <iessa.pragg@gmail.com>
2020-04-02 20:52:50 +1300

    Update file hash to NIO

--- src/main/java/com/superzanti/serversync/util/FileHash.java
+++ src/main/java/com/superzanti/serversync/util/FileHash.java
@@ -1,30 +1,23 @@
 package com.superzanti.serversync.util;
 
+import java.io.BufferedInputStream;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
 
 public class FileHash {
     public static String hashFile(Path file) {
-        try {
-            byte[] fileBytes = Files.readAllBytes(file);
-            MessageDigest hash = MessageDigest.getInstance("SHA-256");
-            hash.update(fileBytes);
-
-            // Digest file
-            byte[] digest = hash.digest();
-            if (digest == null) {
-                Logger.debug(String.format("Failed to digest file: %s", file));
-                return "";
-            }
-
-            // Convert to string
-            StringBuilder sb = new StringBuilder(64);
-            for (byte aDigest : digest) {
-                sb.append(Integer.toString((aDigest & 0xff) + 0x100, 16).substring(1));
-            }
-
-            return sb.toString();
+        try (
+            DigestInputStream in = new DigestInputStream(
+                new BufferedInputStream(Files.newInputStream(file)),
+                MessageDigest.getInstance("SHA-256")
+            )
+        ) {
+            byte[] buffer = new byte[8192];
+            while (in.read(buffer) > -1) { }
+            return String.format("%064x", new BigInteger(1, in.getMessageDigest().digest()));
         } catch (Exception e) {
             Logger.debug(String.format("Failed to hash file: %s", file));
             Logger.debug(e);
