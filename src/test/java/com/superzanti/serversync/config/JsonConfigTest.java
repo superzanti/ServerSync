e247df23e46d36223143f84fd526298c9a89b881
Rheimus <iessa.pragg@gmail.com>
2020-08-05 21:38:42 +1200

    Fix json config tests

--- src/test/java/com/superzanti/serversync/config/JsonConfigTest.java
+++ src/test/java/com/superzanti/serversync/config/JsonConfigTest.java
@@ -1,6 +1,9 @@
 package com.superzanti.serversync.config;
 
+import com.superzanti.serversync.ServerSync;
 import com.superzanti.serversync.SyncConfig;
+import com.superzanti.serversync.util.enums.EServerMode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,12 +14,19 @@ import java.nio.file.Path;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class JsonConfigTest {
+    @BeforeEach()
+    void beforeEach() {
+
+    }
+
     @ParameterizedTest
     @DisplayName("Parsing")
     @ValueSource(strings = {"src/test/resources/server-config.json"})
     void parseServerConfig(Path file) {
+        ServerSync.MODE = EServerMode.SERVER;
         try {
-            SyncConfig config = JsonConfig.forServer(file);
+            JsonConfig.forServer(file);
+            SyncConfig config = SyncConfig.getConfig();
             assertNotNull(config.PUSH_CLIENT_MODS);
             assertNotNull(config.SYNC_MODE);
             assertNotNull(config.SERVER_PORT);
