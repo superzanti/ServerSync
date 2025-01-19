47fd704b46eb292326476f9f02d23e0f4c850b60
Hadyark <32941087+Hadyark@users.noreply.github.com>
2025-01-19 19:49:49 +0100

    fix logo + warning when building

--- build-gui/src/main/java/com/superzanti/serversync/GUI/Gui_JavaFX.java
+++ build-gui/src/main/java/com/superzanti/serversync/GUI/Gui_JavaFX.java
@@ -1,9 +1,11 @@
 package com.superzanti.serversync.GUI;
 
+import com.superzanti.serversync.RefStrings;
 import com.superzanti.serversync.config.SyncConfig;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 // Main class of the GUI, launch the window
@@ -23,13 +25,14 @@ public class Gui_JavaFX extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         root.setStyle(SyncConfig.getConfig().THEME.toString());
 
         Scene scene = new Scene(root, 1024, 576);
         scene.getStylesheets().add(this.getClass().getResource("/css/application.bss").toExternalForm());
         primaryStage.setScene(scene);
-       // primaryStage.setTitle(RefStrings.NAME + " - " + RefStrings.VERSION);
+        primaryStage.setTitle(RefStrings.NAME + " - " + RefStrings.VERSION);
+        primaryStage.getIcons().add(new Image("ServersyncLogo-128.png"));
         primaryStage.setOnCloseRequest(t -> {
             Platform.exit();
             System.exit(0);
